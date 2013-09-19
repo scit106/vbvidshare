c0888f2722c94937955b20b4e94e5a8335c5e843
scit106 <scit106@gmail.com>
2013-09-19 16:03:26 -0400

    initial rails project generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vbvidshare::Application.config.secret_token = '6b963b02df5ad55be181b27b8dba095d2fc3927a7252b0a0b73832e72b96ee6c45e2d0d5b614288130922c47b86410402e20a29851df37d9abb694b0c0ff14ed'
